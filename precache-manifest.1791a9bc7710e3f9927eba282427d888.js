c4fe3c518090bb09848c72b08c00b817d09bbad2
Cole Rudolph <crudolph@gmail.com>
2019-10-29 20:34:43 -0400

    Updates

---
+++ precache-manifest.1791a9bc7710e3f9927eba282427d888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfeeccb11ab3fddf2e5435a2f4a9569",
+    "url": "/Skorgum/Clicky-game/index.html"
+  },
+  {
+    "revision": "d75860104f2551bcf018",
+    "url": "/Skorgum/Clicky-game/static/css/main.e1b0eaa8.chunk.css"
+  },
+  {
+    "revision": "87f0216e96fa8e58fa42",
+    "url": "/Skorgum/Clicky-game/static/js/2.36308ebf.chunk.js"
+  },
+  {
+    "revision": "d75860104f2551bcf018",
+    "url": "/Skorgum/Clicky-game/static/js/main.2a4e998d.chunk.js"
+  },
+  {
+    "revision": "f49db42b60109a13bf49",
+    "url": "/Skorgum/Clicky-game/static/js/runtime-main.33f9d85a.js"
+  }
+]);
